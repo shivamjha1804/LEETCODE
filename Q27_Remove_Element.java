1a1572f51e19f42cda154386780b3e70686d447b
Shivam Jha <jhashivam1804@gmail.com>
2023-04-14 19:04:28 +0530

    LeetCode Solution

---
+++ Q27_Remove_Element.java
@@ -0,0 +1,23 @@
+public class Q27_Remove_Element 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        if (nums == null || nums.length == 0) 
+        {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) 
+        {
+            if (nums[j] != val) 
+            {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        
+        return i;
+    }
+}
