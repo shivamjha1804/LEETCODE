81cf6e8cd01de5644ee32a5036f8af675133bfc7
Shivam Jha <jhashivam1804@gmail.com>
2023-04-14 18:09:41 +0530

    Leet Code Solution

---
+++ Q26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,23 @@
+public class Q26_Remove_Duplicates_from_Sorted_Array
+{
+    public int removeDuplicates(int[] nums) 
+    {
+        if (nums == null || nums.length == 0) 
+        {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) 
+        {
+            if (nums[j] != nums[i]) 
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        
+        return i + 1;
+    }
+}
